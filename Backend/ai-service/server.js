3ea0be046cca89f161379b19a3e8f1b44ba4eb37
Vinya Jain <jainvinya960@gmail.com>
2026-04-29 00:18:08 +0530

    error resolved

--- Backend/ai-service/server.js
+++ Backend/ai-service/server.js
@@ -1,5 +1,5 @@
 const express = require("express"); // Required libraries
-const cors = require("cors");
+const cors = require("cors");// allow frontend request
 const axios = require("axios");
 require("dotenv").config();
 
